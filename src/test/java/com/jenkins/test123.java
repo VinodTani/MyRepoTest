b32f6bc2588ccea342f975f79b7a5d05d21a1024
VinodTani <vinodtanishq009@gmail.com>
2024-08-23 14:32:44 +0530

    My First Commit

---
+++ src/test/java/com/jenkins/test123.java
@@ -0,0 +1,11 @@
+//package com.jenkins;
+//
+//import org.testng.annotations.Test;
+//
+//public class test123 {
+//	@Test
+//	public void verifyTest() {
+//		System.out.println("************* Verify Test Executed Succesfully **************");
+//	}
+//
+//}
---
+++ src/test/java/com/jenkins/testingVinod.java
@@ -0,0 +1,9 @@
+package com.jenkins;
+
+public class testingVinod {
+
+	public static void main(String[] args) {
+		System.out.println(" JENKINS EXECUTION SUCCESSO");
+	}
+
+}
